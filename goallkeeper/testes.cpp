491108ae59e954321bf6c5f2eef98ec6abd84840
nataliagdomingos <164268964+nataliagdomingos@users.noreply.github.com>
2024-07-20 00:56:28 -0300

    Update testes.cpp

--- goallkeeper/testes.cpp
+++ goallkeeper/testes.cpp
@@ -15,3 +15,18 @@ else:
     
     #ajustar a posição do goleiro coma bola
     self.robot.setSpin(spinGoalKeeper(rb, rr, rg))
+
+
+ def avoidPlayers(self):
+    avoidance_radius = 0.05  # Raio de detecção de colisão
+    repulsion_strength = 0.1  # Força de repulsão para desviar dos jogadores
+
+    rr = np.array(self.robot.pos)  # Posição do robô goleiro
+    vr = np.array(self.robot.v)  # Velocidade do robô goleiro
+
+    # jogadores adversários
+    for player in self.world.opponents:
+        rp = np.array(player.pos)  # Posição do jogador adversário
+        dist_robot_player = np.linalg.norm(rr - rp)  # Distância entre o robô e o jogador
+
+        
